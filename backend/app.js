e1fa31572ae45f55ec97941918cab2763555251e
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2022-08-03 22:26:05 +0300

    app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
+import router from "./routes/user-routes";
+
 const app = express();
+
+app.use("/api/user", router);
 
 mongoose
   .connect(
