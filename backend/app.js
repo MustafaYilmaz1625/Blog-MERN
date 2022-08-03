c18dded5e1ab5ff154133de8e4af84de8e654bdb
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2022-08-03 15:46:24 +0300

    moonges

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,11 @@
-console.log("hello");
+import express from "express";
+import mongoose from "mongoose";
+const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://codingwithmustafa:test1234@cluster0.3vfzmjn.mongodb.net/Blog?retryWrites=true&w=majority"
+  )
+  .then(() => app.listen(5000))
+  .then(() => console.log("connected To database"))
+  .catch((err) => console.log(err));
