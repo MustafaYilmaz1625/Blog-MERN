0ebef378c2ad790e1f099f3a85100d0859ae68a6
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2022-08-13 19:00:44 +0300

    home and home.css

---
+++ react.instagram/src/pages/home/Home.jsx
@@ -0,0 +1,17 @@
+import "./home.css";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="home-page">
+          awffaf
+          <div className="home-page-left"></div>
+          <div className="home-page-right"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
