c46e5bb0baa5d7aabafa8118f5cfde9760eb1eb4
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2022-08-15 15:52:28 +0300

    header install

--- react.instagram/src/components/Logo/Logo.jsx
+++ react.instagram/src/components/Logo/Logo.jsx
@@ -4,6 +4,7 @@ const Logo = () => {
   return (
     <a href="/" style={{ display: "flex" }}>
       <img
+        style={{ height: "28px", width: "103px" }}
         src="https://www.instagram.com/static/images/web/logged_out_wordmark-2x.png/d2529dbef8ed.png"
         alt="logo"
       />
--- react.instagram/src/components/header/Header.jsx
+++ react.instagram/src/components/header/Header.jsx
@@ -1,20 +1,41 @@
 import React from "react";
 import Logo from "../Logo/Logo";
 import SearchIcon from "@mui/icons-material/Search";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import ChatOutlinedIcon from "@mui/icons-material/ChatOutlined";
+import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
+import { Avatar } from "@mui/material";
+
 import "./header.css";
 
 const Header = () => {
   return (
     <div className="header-wrapper">
       <div className="container">
-        <div className="header"></div>
-        <Logo />
-        <div className="search">
-          <SearchIcon className="search-icon" />
-          <input type="text " placeholder="Search" />
-        </div>
-        <div className="header-lis">
-          <a href="/"></a>
+        <div className="header">
+          <Logo />
+          <div className="search">
+            <SearchIcon className="search-icon" />
+            <input type="text " placeholder="Search" />
+          </div>
+          <div className="header-links">
+            <a href="/">
+              <HomeOutlinedIcon className="icon" />
+            </a>
+            <a href="/">
+              <ChatOutlinedIcon className="icon" />
+            </a>
+            <a href="/">
+              <AddBoxOutlinedIcon className="icon" />
+            </a>
+            <a href="/">
+              <Avatar
+                alt="Remy Sharp"
+                src="https://res.cloudinary.com/techprivil/image/upload/v1655937337/instagram-clone/bhd2k9blnfvdrpismvy0.jpg"
+                sx={{ width: 28, height: 28 }}
+              />
+            </a>
+          </div>
         </div>
       </div>
     </div>
