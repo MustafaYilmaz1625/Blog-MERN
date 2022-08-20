52b8efda304d13e640f6f92454c0f2e0e5586c14
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2022-08-20 21:51:14 +0300

    rightBox

--- react.instagram/src/components/rightBox/RightBox.jsx
+++ react.instagram/src/components/rightBox/RightBox.jsx
@@ -1,7 +1,48 @@
+import { Avatar, Button } from "@mui/material";
+import ArrowCircleRightOutlinedIcon from "@mui/icons-material/ArrowCircleRightOutlined";
+
 import "./rightBox.css";
 
 const RightBox = () => {
-  return <div>RightBox</div>;
+  return (
+    <div className="right-box">
+      <div className="account">
+        <a href="/">
+          <Avatar src="/images/person/0.png" sx={{ width: 56, height: 56 }} />
+        </a>
+        <a href="/" className="account-titles">
+          <a href="/">eminbasbayan</a>
+          <span>Emin Başbayan</span>
+        </a>
+      </div>
+      <span className="friends-title">My Friends</span>
+      <ul className="friends-list">
+        <li className="friend-item">
+          <div className="friend-item-left">
+            <a href="/">
+              <Avatar
+                src="/images/person/2.png"
+                sx={{ width: 40, height: 40 }}
+              />
+            </a>
+            <div className="friend-username">
+              <a href="/">bedirhan</a>
+              <span>Bedirhan Başbayan</span>
+            </div>
+          </div>
+          <div className="friend-item-right">
+            <Button
+              variant="contained"
+              size="small"
+              endIcon={<ArrowCircleRightOutlinedIcon />}
+            >
+              Profile Git
+            </Button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default RightBox;
