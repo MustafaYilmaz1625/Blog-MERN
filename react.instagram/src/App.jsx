a642d56376eda1d5ab2ccce12c28657836c05c30
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2022-09-11 20:04:30 +0300

    responsive

--- react.instagram/src/App.jsx
+++ react.instagram/src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import Header from "./components/header/Header";
 import Share from "./components/share/Share";
-import Login from "./pages/login/Login";
+// import Login from "./pages/login/Login";
 // import Register from "./pages/register/Register";
 // import Messenger from "./pages/messenger/Messenger";
-import Home from "./pages/home/Home";
-// import Profile from "./pages/profile/Profile";
+// import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
 
 function App() {
   const [open, setOpen] = useState(false);
@@ -15,8 +15,8 @@ function App() {
     <div className="App">
       <Header handleOpen={handleOpen} />
       <Share open={open} handleClose={handleClose} />
-      <Home />
-      {/* <Profile /> */}
+      {/* <Home /> */}
+      <Profile />
       {/* <Messenger/> */}
       {/* <Register/> */}
       {/* <Login/> */}
