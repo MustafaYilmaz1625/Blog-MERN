b5d2178d368e8dde445ebb3a86d9787aeb182b3c
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2022-09-30 21:42:30 +0300

    react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import Header from "./components/header/Header";
 import Share from "./components/share/Share";
-// import Login from "./pages/login/Login";
-// import Register from "./pages/register/Register";
-// import Messenger from "./pages/messenger/Messenger";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Messenger from "./pages/messenger/Messenger";
 import Home from "./pages/home/Home";
-// import Profile from "./pages/profile/Profile";
+import Profile from "./pages/profile/Profile";
+import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
 
 function App() {
   const [open, setOpen] = useState(false);
@@ -13,13 +14,17 @@ function App() {
   const handleClose = () => setOpen(false);
   return (
     <div className="App">
-      <Header handleOpen={handleOpen} />
       <Share open={open} handleClose={handleClose} />
-      <Home />
-      {/* <Profile /> */}
-      {/* <Messenger/> */}
-      {/* <Register/> */}
-      {/* <Login/> */}
+      <Router>
+        {<Header handleOpen={handleOpen} />}
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/profile/:username" element={<Profile />}/>
+          <Route path="/messenger" element={<Messenger />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/register" element={<Register />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
