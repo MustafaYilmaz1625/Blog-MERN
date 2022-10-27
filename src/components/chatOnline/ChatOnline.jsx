b5367163c90854df18da772bd2e6109d156b7b9e
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2022-10-28 00:17:08 +0300

    chatonline

--- src/components/chatOnline/ChatOnline.jsx
+++ src/components/chatOnline/ChatOnline.jsx
@@ -1,18 +1,38 @@
 import { Avatar } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import "./chatOnline.css";
 
- const ChatOnline = () => {
-    return (
-        <div className="chat-online">
-            <div className="chat-online-friend">
-                <div className="chat-online-img-container">
-                    <Avatar src="/images/person/2.png" />
-                    <span className="chat-online-badge"></span>
-                </div>
-                <div className="chat-online-name">bedirhan</div>
-            </div>
-        </div>
-    );
-};
+export const ChatOnline = ({ onlineUsers, currentId, setCurrentChat }) => {
+  const [friends, setFriends] = useState([]);
+  const [onlineFriends, setOnlineFriends] = useState([]);
 
-export default ChatOnline
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+
+  useEffect(() => {
+    const getFriends = async () => {
+      const res = await axios.get("/users/friends/" + currentId);
+      setFriends(res.data);
+    };
+    getFriends();
+  }, [currentId]);
+
+  useEffect(() => {
+    setOnlineFriends(friends.filter((f) => onlineUsers.includes(f._id)));
+  }, [friends, onlineUsers]);
+
+  return (
+    <div className="chat-online">
+      {onlineFriends.map((o) => (
+        <div className="chat-online-friend" key={o._id}>
+          <div className="chat-online-img-container">
+            <Avatar src={o.profilePicture && PF + o.profilePicture} />
+            <span className="chat-online-badge"></span>
+          </div>
+          <div className="chat-online-name">{o.username}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
+export default ChatOnline;
