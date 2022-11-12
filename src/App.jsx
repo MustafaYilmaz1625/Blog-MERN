0ade4d8b77213da63bd25f47edc10ded452c1d54
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2022-11-12 15:24:14 +0300

    add update install app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,28 @@
 import { useContext, useState } from "react";
-import Header from "./components/header/Header";
-import Share from "./components/share/Share";
-import Login from "./pages/login/Login";
-import Register from "./pages/register/Register";
-import Messenger from "./pages/messenger/Messenger";
-import Home from "./pages/home/Home";
-import Profile from "./pages/profile/Profile";
-import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
-
-import {ToastContainer} from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
-import { AuthContext } from "./context/AuthContext";
+import  Header  from "./components/header/Header";
+import  Share  from "./components/share/Share";
+import  Login  from "./pages/login/Login";
+import  Register  from "./pages/register/Register";
+import  Messenger  from "./pages/messenger/Messenger";
+import  Home  from "./pages/home/Home";
+import Profile  from "./pages/profile/Profile";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import  AuthContext  from "./context/AuthContext";
 
 function App() {
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
-  const {user} = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
   return (
     <div className="App">
       <Share open={open} handleClose={handleClose} />
-      <ToastContainer/>
+      <ToastContainer />
       <Router>
-        {user &&  <Header handleOpen={handleOpen} />}
+        {user && <Header handleOpen={handleOpen} />}
         <Routes>
           <Route path="/" element={user ? <Home /> : <Login />} />
           <Route
